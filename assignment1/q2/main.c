ac2543f9a449a3dc556c70b1d24429b051bbb73d
Serge Mezui <sergemezui.dev@gmail.com>
2024-09-24 16:21:41 +0200

    solve second question of assignment1

---
+++ assignment1/q2/main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+
+/**
+ * write a program to add four numbers execute the values 
+ * of each variable, the sum, the average of those four 
+ * numbers of double datatypes.
+ */
+int main(int argc, char const *argv[])
+{
+    double num1, num2, num3, num4;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    printf("Enter the third number: ");
+    scanf("%lf", &num3);
+
+    printf("Enter the last number: ");
+    scanf("%lf", &num4);
+
+    double sum = num1 + num2 + num3 + num4;
+    double avg = sum / 4;
+
+    printf("\n\nHere are the SUM and AVG of your numbers:\n");
+    printf("SUM: %lf \n", sum);
+    printf("AVG: %lf", avg);
+
+    return 0;
+}
